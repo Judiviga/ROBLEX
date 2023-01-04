624b6b26991ad6302c161f89b8a5d8f6d5c78665
Judiviga <88287673+Judiviga@users.noreply.github.com>
2023-01-03 21:53:24 -0500

    Update puertos_digitales_salidas.ino

--- examples/puertos_digitales_salidas/puertos_digitales_salidas.ino
+++ examples/puertos_digitales_salidas/puertos_digitales_salidas.ino
@@ -13,7 +13,7 @@ void setup() {
   Serial.begin(115200); // iniciar debug serial
   Serial.println("start");
 
-  pcf.begin(0x20, &Wire); // iniciar pcf 
+  pcf.begin(0x27, &Wire); // iniciar pcf 
 
   for (int p = 1; p <= 6; p++) {  // configurar los 6 puertos como salidas
     pcf.pinMode(p, OUTPUT);
