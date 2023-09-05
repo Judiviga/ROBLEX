79d674669c4b0945dcc840f8925af0a383c13fca
Judiviga <88287673+Judiviga@users.noreply.github.com>
2023-09-04 22:28:18 -0500

    Update BL_esclavo.ino

--- examples/comunicaciones/BL_esclavo/BL_esclavo.ino
+++ examples/comunicaciones/BL_esclavo/BL_esclavo.ino
@@ -5,7 +5,8 @@ BluetoothSerial SerialBT;
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ROBLEX SLAVE"); //Nombre del dispositivo
-
+  
+  Serial.println(SerialBT.getBtAddressString()); //imprimir el address del dispositivo
 }
 
 void loop() {
