56feb1843a98e05957d6c1c04e8126174cb6b008
Judiviga <88287673+Judiviga@users.noreply.github.com>
2025-06-11 10:47:56 -0500

    Update puertos_digitales_entradas.ino

--- examples/ejemplos_robot/puertos_digitales_entradas/puertos_digitales_entradas.ino
+++ examples/ejemplos_robot/puertos_digitales_entradas/puertos_digitales_entradas.ino
@@ -14,16 +14,16 @@ void setup() {
   Serial.begin(115200);  // iniciar debug serial
   Serial.println("start");
 
-  pcf.begin(0x27, &Wire);  // iniciar pcf
+  pcf.begin(0x3F, &Wire);  // iniciar pcf
 
-  for (int p = 1; p <= 6; p++) {
+  for (int p = 0; p <= 7; p++) {
     pcf.pinMode(p, INPUT_PULLUP);  // configurar los 6 puertos como entradas encendidas por default
   }
 }
 
 void loop() {
 
-  for (int p = 1; p <= 6; p++) {
+  for (int p = 0; p <= 7; p++) {
     Serial.print(pcf.digitalRead(p));  // leer la entrada respectiva e imprimir en el debug serial
     Serial.print("   ");
   }
